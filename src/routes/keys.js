dbbfb2af62bf5da8e8ec52863798592abff4bd56
helyezerteofilo <helyezerteofilo2@hotmail.com>
2024-09-05 03:53:33 -0300

    feat: melhorias e adição de endpoints

--- src/routes/keys.js
+++ src/routes/keys.js
@@ -1,31 +1,43 @@
-const express = require('express');
-const crypto = require('crypto');
-const User = require('../models/User');
-const authenticateToken = require('../middleware/authenticateToken');
+const express = require("express");
+const crypto = require("crypto");
+const User = require("../models/User");
+const authenticateToken = require("../middleware/authenticateToken");
 const router = express.Router();
 
-router.post('/keys', authenticateToken, async (req, res) => {
-    try {
-        const { publicKey, privateKey } = crypto.generateKeyPairSync('rsa', {
-            modulusLength: 2048,
-            publicKeyEncoding: { type: 'pkcs1', format: 'pem' },
-            privateKeyEncoding: { type: 'pkcs1', format: 'pem' },
-        });
+router.post("/keys", authenticateToken, async (req, res) => {
+  try {
+    const { publicKey, privateKey } = crypto.generateKeyPairSync("rsa", {
+      modulusLength: 2048,
+      publicKeyEncoding: { type: "pkcs1", format: "pem" },
+      privateKeyEncoding: { type: "pkcs1", format: "pem" },
+    });
 
-        const user = await User.findByIdAndUpdate(req.user.id, { publicKey, privateKey }, { new: true });
-        res.status(201).json({ message: 'Chaves geradas com sucesso', publicKey: user.publicKey });
-    } catch (error) {
-        res.status(500).json({ error: 'Erro ao gerar chaves' });
-    }
+    const user = await User.findByIdAndUpdate(
+      req.user.id,
+      { publicKey, privateKey },
+      { new: true }
+    );
+    res
+      .status(201)
+      .json({
+        message: "Chaves geradas com sucesso",
+        publicKey: user.publicKey,
+      });
+  } catch (error) {
+    res.status(500).json({ error: "Erro ao gerar chaves" });
+  }
 });
 
-router.delete('/keys', authenticateToken, async (req, res) => {
-    try {
-        const user = await User.findByIdAndUpdate(req.user.id, { publicKey: null, privateKey: null });
-        res.status(200).json({ message: 'Chaves deletadas com sucesso' });
-    } catch (error) {
-        res.status(500).json({ error: 'Erro ao deletar chaves' });
-    }
+router.delete("/keys", authenticateToken, async (req, res) => {
+  try {
+    const user = await User.findByIdAndUpdate(req.user.id, {
+      publicKey: null,
+      privateKey: null,
+    });
+    res.status(200).json({ message: "Chaves deletadas com sucesso" });
+  } catch (error) {
+    res.status(500).json({ error: "Erro ao deletar chaves" });
+  }
 });
 
 module.exports = router;
--- src/server.js
+++ src/server.js
@@ -1,19 +1,22 @@
-require('dotenv').config();
-const express = require('express');
-const mongoose = require('mongoose');
+require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
 const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
 
-mongoose.connect(process.env.MONGO_URI)
-.then(() => console.log('Conectado ao MongoDB'))
-.catch(err => console.log(err));
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => console.log("Conectado ao MongoDB"))
+  .catch((err) => console.log(err));
 
-app.use('/api/auth', require('./routes/auth'));
-app.use('/api', require('./routes/keys'));
-app.use('/api', require('./routes/documents'));
+app.use(cors());
+app.use("/api/auth", require("./routes/auth"));
+app.use("/api", require("./routes/keys"));
+app.use("/api", require("./routes/documents"));
 
 app.listen(PORT, () => {
-    console.log(`Servidor na porta: ${PORT}`);
+  console.log(`Servidor na porta: ${PORT}`);
 });
